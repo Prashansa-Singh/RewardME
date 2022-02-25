b2f8b83516c5e39602f19d42f66dc7a7d0e601f6
Leo <leolin.360@gmail.com>
2022-02-25 19:37:59 +1100

    Added routing to different pages

--- rewardme-react-stuffs/src/App.js
+++ rewardme-react-stuffs/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+// react router
+import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
+
+// pages
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+// This is the router for the whole website
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* These are the individual routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
