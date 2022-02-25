36f6e955dda3d3acea3c2bab47439a3e6aebc115
yipp-217 <76835755+yipp-217@users.noreply.github.com>
2022-02-26 09:31:09 +1100

    Test rewardnode api

--- rewardnode/app.js
+++ rewardnode/app.js
@@ -1,5 +1,5 @@
 /***
- *   RewardME app
+ *   RewardME app/api
  *
  * */
 
@@ -7,10 +7,45 @@ const express = require('express');
 const {request, response} = require("express");
 const app = express();
 
-/* request: incoming data
- * response: outgoing data
+
+/**
+ * testing api functions
+ * using localhost9876
+ *
+ *  * request: incoming data
+ *  * response: outgoing data
+ *
+ *  use Fetch API to fetch
+ *  https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+ *
+ *  APIs:
+ *  - Authentication
+ *  - Get cards
+ *
+ *  Visualise
+ *  open two terminal commands and navigate both into `rewardnode`
+ *  - terminal1 type in: node app.js
+ *      this spawn up the app and start to listen
+ *  - terminal2 type in: curl localhost:9876/test
+ *      an url address specified
  * */
+
+app.listen(9876, () => {
+
+    console.log("Hi John!");
+
+})
+
+
 app.get('/', (request, response) => {
 
+    response.send("Hello Teebana");
+
+});
+
+
+app.get('/test', (request, response) => {
+
+    response.send("Hello Ed");
 
 });
