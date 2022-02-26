25e471123ac4c21125ad17c033df2cef2c705463
CaitlynAllan <88321877+CaitlynAllan@users.noreply.github.com>
2022-02-26 12:36:51 +1100

    Moved location of logos

--- rewardreact/src/pages/Home.js
+++ rewardreact/src/pages/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
+import image from "../res/RewardsME Logo R.png";
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <img 
+        src={image} 
+        alt="R logo"
+      />
     </div>
   );
 };
