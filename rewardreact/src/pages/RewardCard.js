1cdb34d958a70bc97ae73031d5d8cf1d61209be4
CaitlynAllan <88321877+CaitlynAllan@users.noreply.github.com>
2022-02-26 12:17:51 +1100

    Update RewardCard.js

--- rewardreact/src/pages/RewardCard.js
+++ rewardreact/src/pages/RewardCard.js
@@ -36,20 +36,14 @@ export default class RewardCard extends React.Component {
   }
 }
 
-function MediaControlCard(props) {
+function MediaControlCard() {
   const theme = useTheme();
-  const { id, name, img } = props;
   return (
     <Grid item xs={12}>
       <Item>
       <Card sx={{ maxWidth: 345 }}>
           <CardActionArea>
-            <CardMedia component="img" height="200" image={img} alt="" />
-            <CardContent>
-              <Typography variant="h6" component="div">
-                {name}
-              </Typography>
-            </CardContent>
+            <CardMedia component="img" image="https://lh3.googleusercontent.com/fife/AAWUweVKtJQawZJjUQHHl5cOkC39kLmy9sus6raYE_h942Il7QCHl3geOUxr1tsa64hvu0u_tdYagRrR0PXdF2qraRqG1VeMqia_nflEUwYz-f9Z6RTu_9KGLkzG2y4mpmdKBBFpY2qaU0HUHozhUbOy6yqFtTjeMCCms6soa1hyIaccKYTdZFhWL4kcODyTD_fxAa2N1gdBi3m1jIjEuzTiqr4TKO8oQTedyi6O88pgD_kfVPyoWpKBxT2l-i-Jf3zAlaK_oZ4Wx7Tyb746LqcbCOlyoilYPqFgc4yBDl8ElDgLnr0R5TzpZ6_WaE8FzVz1YFn_VrY25HvZ5C20vs8g3qOSR0p5kXSmfasx_VAtWxRRWykYe4uUiav4W5YMOnCuYGEtD28wFpLRhEN3FpmTEWkrUL0JL5WrIlOcNLe3E3wjwz-a4SMJeqGcRI6w11Q_a9nxXDtGLPkaPsrwC3Jc4jfaUaCksFHS5eaEdjBw_FGZGB-dN3BXQ4b-nOW1Smj6JuN0QKqxXHdE27enc30hO3gJF5Lo2oxa1OYDGiop6ERmbAd-QVCLp7F5902tNaju83VQgGhCZjmN1v4ifY6ls1toHCuoc6csTrquxYDV0o1aYx0zUqWEF3a3wvRiJokCAHJNoR9c1W5GFR_hPnWRQJLzgYVvVXZ2wvHzaRI5wUYC9cagjwEv8DIWzDCE3Zl7AnOGMwe4EapYIQrf2fe82GF_L92Pr7zuPuo=w1133-h1356" alt="" />
           </CardActionArea>
         </Card>
       </Item>
